ad2761b9921b6e6cfcb69e5251eadc0bc0ba1708
ditramadia <ditrarizqaamadia@gmail.com>
2024-06-08 03:15:45 +0700

    feat: add banner for small screen

--- src/app/ideas/page.tsx
+++ src/app/ideas/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import Navbar from "@components/navigation/Navbar";
+import Banner from "@components/banner/Banner";
 
 const Ideas = () => {
   return (
     <main className="max-w-screen h-[200vh]">
       <Navbar active="Ideas"/>
+      <Banner />
     </main>
   );
 };
---
+++ src/components/banner/Banner.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+
+const Banner = () => {
+
+  const [bannerImage, setBannerImage] = useState<string>('');
+
+  useEffect(() => {
+    // Fetch banner image here
+    const fetchBannerImage = () => {
+      setBannerImage('https://images.pexels.com/photos/196645/pexels-photo-196645.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+    }
+
+    fetchBannerImage();
+  }, [])
+
+  return (
+    <div className="relative w-full h-[420px]">
+      <div 
+        className="absolute w-full h-full bg-cover bg-center bg-no-repeat transform skew-y-[-12deg] origin-top-left" 
+        style={{backgroundImage: "url('"+bannerImage+")"}} 
+      >
+        <div className="absolute inset-0 bg-scrim/70" />
+      </div>
+      <div className="relative w-full h-full flex flex-col justify-center items-center">
+        <h1 className="heading-1 text-neutral-100 tracking-widest">Ideas</h1>
+        <h2 className="heading-5 text-neutral-100">Where all our great things begin</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
